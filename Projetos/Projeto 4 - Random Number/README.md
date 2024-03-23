db89d0aeeb3d82a0763d0595548cf5e95738aa43
Amanda Gois <amandabalcacar@gmail.com>
2024-03-23 00:12:46 -0400

    feat: logica ok

---
+++ Projetos/Projeto 4 - Random Number/README.md
@@ -0,0 +1,20 @@
+# Gerador de Cores Aleatórias
+
+Este é um projeto simples de um site que gera uma cor aleatória ao pressionar um botão e muda a cor de fundo do site para essa cor gerada.
+
+## Funcionalidades
+
+- Gera uma cor aleatória ao pressionar o botão.
+- Altera a cor de fundo do site para a cor aleatória gerada.
+
+## Como Usar
+
+1. Abra o site em um navegador da web.
+2. Clique no botão "Gerar Cor" para obter uma cor aleatória.
+3. Observe como a cor de fundo do site muda para a cor gerada.
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
---
+++ Projetos/Projeto 4 - Random Number/index.js
@@ -0,0 +1,25 @@
+let computerNumber
+let userNumbers = []
+
+
+function init() {
+    computerNumber = Math.floor(Math.random() * 100 + 1 )
+    console.log(computerNumber)
+}
+
+function compareNumbers() {
+    const userNumber = Number(document.getElementById('inputBox').value)
+    userNumbers.push(' ' + userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
+
+    if(userNumber > computerNumber){
+        document.getElementById('textOutput').innerHTML = 'Your number is too high'
+    }
+    else if (userNumber < computerNumber){
+        document.getElementById('textOutput').innerHTML = 'Your number is too low'
+    }
+    else{
+        document.getElementById('textOutput').innerHTML = 'Congratulation'
+    }
+}
+
