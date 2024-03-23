3280d7226a8df573772b8eb1df46115751767691
Amanda Gois <amandabalcacar@gmail.com>
2024-03-22 21:28:33 -0400

    feat: formatMoney and people e person

--- Projetos/Projeto 2 - Calculadora de Gorjeta/index.js
+++ Projetos/Projeto 2 - Calculadora de Gorjeta/index.js
@@ -1,6 +1,12 @@
-function formatMoney(){
+function formatMoney(value){
+    value = Math.ceil(value * 100) / 100
     value = value.toFixed(2)
     return '$ ' + value
+}
+
+function formatSplit(value){
+    if (value == 1) return value + ' person'
+    return value + ' people'
 }
 
 
