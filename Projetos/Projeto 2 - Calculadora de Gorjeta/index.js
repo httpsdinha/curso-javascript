c22702e42ce10314057d5c21e1a33958b44a89e9
Amanda Gois <amandabalcacar@gmail.com>
2024-03-22 20:29:00 -0400

    feat: atualização variaveis

--- Projetos/Projeto 2 - Calculadora de Gorjeta/index.js
+++ Projetos/Projeto 2 - Calculadora de Gorjeta/index.js
@@ -3,5 +3,14 @@ function update(){
     let tipPercent = document.getElementById('tipInput').value
     let split = document.getElementById('splitInput').value
 
-    console.log({bill,tipPercent,split})
+    let tipValue = bill * (tipPercent/100)
+    let billtotal = bill + tipValue
+    let billEach = billtotal / split
+
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %0'
+    document.getElementById('tipValue').innerHTML = '$ ' + tipValue
+    document.getElementById('totalWithTip').innerHTML = '$ ' + billtotal
+    document.getElementById('splitValue').innerHTML = split 
+    document.getElementById('billEach').innerHTML = billtotal / split
+
 }
