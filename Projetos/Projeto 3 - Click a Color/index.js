8b4b6d7706397f2ef160c667c6cb3b2b37f2f086
Amanda Gois <amandabalcacar@gmail.com>
2024-03-22 22:21:48 -0400

    fix: alteração de cor indicada corrigida

--- Projetos/Projeto 3 - Click a Color/index.js
+++ Projetos/Projeto 3 - Click a Color/index.js
@@ -1,2 +1,9 @@
-const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
+const colors = ['green', 'yellow', 'purple', 'blue', 'red']
 const btn = document.getElementById('btn')
+
+// escutar por um evento
+btn.addEventListener('click', function() {
+    // troca o style background para colors
+    document.body.style.background = colors[2]
+    
+})
