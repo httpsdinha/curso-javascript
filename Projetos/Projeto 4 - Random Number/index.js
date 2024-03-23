5043d36eb274b5d5715ef468fc3e62a1fef28b03
Amanda Gois <amandabalcacar@gmail.com>
2024-03-23 00:19:26 -0400

    refactor: numero de tenativas adicionado

--- Projetos/Projeto 4 - Random Number/index.js
+++ Projetos/Projeto 4 - Random Number/index.js
@@ -1,6 +1,6 @@
 let computerNumber
 let userNumbers = []
-
+let attempts = 0
 
 function init() {
     computerNumber = Math.floor(Math.random() * 100 + 1 )
@@ -14,11 +14,15 @@ function compareNumbers() {
 
     if(userNumber > computerNumber){
         document.getElementById('textOutput').innerHTML = 'Your number is too high'
-        document.getElementById('inputBox').value = ''
+        document.getElementById('inputBox').value = ' '
+        attempts++
+        document.getElementById('attempts').innerHTML = attempts
     }
     else if (userNumber < computerNumber){
         document.getElementById('textOutput').innerHTML = 'Your number is too low'
-        document.getElementById('inputBox').value = ''
+        document.getElementById('inputBox').value = ' '
+        attempts++
+        document.getElementById('attempts').innerHTML = attempts
     }
     else{
         document.getElementById('textOutput').innerHTML = 'Congratulation'
