8633e85463e8067e4d96e92caa39d1b22867481b
Amanda Gois <amandabalcacar@gmail.com>
2024-03-22 22:46:25 -0400

    feat: projeto 2 final

--- Projetos/Projeto 3 - Click a Color/index.js
+++ Projetos/Projeto 3 - Click a Color/index.js
@@ -2,8 +2,18 @@ const colors = ['green', 'yellow', 'purple', 'blue', 'red']
 const btn = document.getElementById('btn')
 
 // escutar por um evento
+
+
 btn.addEventListener('click', function() {
+    const randomColor = getRandomColor()
+    console.log(randomColor)
     // troca o style background para colors
-    document.body.style.background = colors[2]
-    
+    document.body.style.background = colors[getRandomColor()]
+
 })
+
+
+function getRandomColor(){
+    return Math.floor(Math.random() * colors.length)
+}
+
